f67a69dfe2d2c4357271f8b1c7c01b33ccd802a4
Dev-Navi <17naveen98@gmail.com>
2024-06-23 17:37:30 +0530

    nvn

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -186,31 +186,6 @@ export default function Register() {
                   <div className="grid grid-cols-2 gap-2">
                     <div>
                       <label
-                        for="confirmpassword"
-                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                      >
-                        Confirm Password
-                      </label>
-                      <input
-                        type="password"
-                        name="confirmpassword"
-                        id="confirmpassword"
-                        placeholder="••••••••"
-                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                        required=""
-                        onChange={handleRegChange}
-                      />
-                      {validateError.confirmpassword && (
-                        <span
-                          className="text-danger"
-                          style={{ fontSize: "13px" }}
-                        >
-                          {validateError.confirmpassword}
-                        </span>
-                      )}
-                    </div>
-                    <div>
-                      <label
                         for="password"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                       >
@@ -231,6 +206,31 @@ export default function Register() {
                           style={{ fontSize: "13px" }}
                         >
                           {validateError.password}
+                        </span>
+                      )}
+                    </div>
+                    <div>
+                      <label
+                        for="confirmpassword"
+                        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      >
+                        Confirm Password
+                      </label>
+                      <input
+                        type="password"
+                        name="confirmpassword"
+                        id="confirmpassword"
+                        placeholder="••••••••"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        required=""
+                        onChange={handleRegChange}
+                      />
+                      {validateError.confirmpassword && (
+                        <span
+                          className="text-danger"
+                          style={{ fontSize: "13px" }}
+                        >
+                          {validateError.confirmpassword}
                         </span>
                       )}
                     </div>
