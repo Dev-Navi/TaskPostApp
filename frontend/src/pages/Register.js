9fea9289c1fb4701fc20844efd2657422484657d
Dev-Navi <17naveen98@gmail.com>
2024-07-01 14:44:57 +0530

    d

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -31,6 +31,8 @@ let initialstate = {
 export default function Register() {
   const [formdata, setformdata] = useState(initialstate);
   const [validateError, setValidateError] = useState({});
+  const [showP, setShowP] = useState(false);
+  const [showP1, setShowP1] = useState(false);
 
   const { email, confirmpassword, name, role, password, phone } = formdata;
   console.log(formdata, "formdata");
@@ -114,6 +116,13 @@ export default function Register() {
     }
   };
 
+  const handleShow = (sh) => {
+    sh ? setShowP(false) : setShowP(true);
+  };
+  const handleShow1 = (sh) => {
+    sh ? setShowP1(false) : setShowP1(true);
+  };
+
   return (
     <div>
       <Container>
@@ -149,10 +158,7 @@ export default function Register() {
                         onChange={handleRegChange}
                       />
                       {validateError.name && (
-                        <span
-                          className="text-danger"
-                          style={{ fontSize: "13px" }}
-                        >
+                        <span className="text-danger">
                           {validateError.name}
                         </span>
                       )}
@@ -190,9 +196,101 @@ export default function Register() {
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                       >
                         Password
+                        <div
+                          style={{
+                            position: "absolute",
+                            color: "green",
+                            float: "right",
+                            marginLeft: "10pc",
+                            marginTop: "-13px",
+                          }}
+                          onClick={() => handleShow(showP)}
+                        >
+                          {showP ? (
+                            <svg
+                              width="12px"
+                              height="12px"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                              <g
+                                id="SVGRepo_tracerCarrier"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              ></g>
+                              <g id="SVGRepo_iconCarrier">
+                                {" "}
+                                <path
+                                  d="M2 2L22 22"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></path>{" "}
+                                <path
+                                  d="M6.71277 6.7226C3.66479 8.79527 2 12 2 12C2 12 5.63636 19 12 19C14.0503 19 15.8174 18.2734 17.2711 17.2884M11 5.05822C11.3254 5.02013 11.6588 5 12 5C18.3636 5 22 12 22 12C22 12 21.3082 13.3317 20 14.8335"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></path>{" "}
+                                <path
+                                  d="M14 14.2362C13.4692 14.7112 12.7684 15.0001 12 15.0001C10.3431 15.0001 9 13.657 9 12.0001C9 11.1764 9.33193 10.4303 9.86932 9.88818"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></path>{" "}
+                              </g>
+                            </svg>
+                          ) : (
+                            <svg
+                              width="12px"
+                              height="12px"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                              <g
+                                id="SVGRepo_tracerCarrier"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              ></g>
+                              <g id="SVGRepo_iconCarrier">
+                                {" "}
+                                <path
+                                  d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></path>{" "}
+                                <path
+                                  d="M1 12C1 12 5 20 12 20C19 20 23 12 23 12"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></path>{" "}
+                                <circle
+                                  cx="12"
+                                  cy="12"
+                                  r="3"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></circle>{" "}
+                              </g>
+                            </svg>
+                          )}
+                        </div>
                       </label>
                       <input
-                        type="password"
+                        type={`${showP ? "text" : "password"}`}
                         name="password"
                         id="password"
                         placeholder="••••••••"
@@ -214,10 +312,102 @@ export default function Register() {
                         for="confirmpassword"
                         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                       >
-                        Confirm Password
+                        Confirm Password{" "}
+                        <div
+                          style={{
+                            position: "absolute",
+                            color: "green",
+                            float: "right",
+                            marginLeft: "10pc",
+                            marginTop: "-13px",
+                          }}
+                          onClick={() => handleShow1(showP1)}
+                        >
+                          {showP1 ? (
+                            <svg
+                              width="12px"
+                              height="12px"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                              <g
+                                id="SVGRepo_tracerCarrier"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              ></g>
+                              <g id="SVGRepo_iconCarrier">
+                                {" "}
+                                <path
+                                  d="M2 2L22 22"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></path>{" "}
+                                <path
+                                  d="M6.71277 6.7226C3.66479 8.79527 2 12 2 12C2 12 5.63636 19 12 19C14.0503 19 15.8174 18.2734 17.2711 17.2884M11 5.05822C11.3254 5.02013 11.6588 5 12 5C18.3636 5 22 12 22 12C22 12 21.3082 13.3317 20 14.8335"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></path>{" "}
+                                <path
+                                  d="M14 14.2362C13.4692 14.7112 12.7684 15.0001 12 15.0001C10.3431 15.0001 9 13.657 9 12.0001C9 11.1764 9.33193 10.4303 9.86932 9.88818"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></path>{" "}
+                              </g>
+                            </svg>
+                          ) : (
+                            <svg
+                              width="12px"
+                              height="12px"
+                              viewBox="0 0 24 24"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                              <g
+                                id="SVGRepo_tracerCarrier"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                              ></g>
+                              <g id="SVGRepo_iconCarrier">
+                                {" "}
+                                <path
+                                  d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></path>{" "}
+                                <path
+                                  d="M1 12C1 12 5 20 12 20C19 20 23 12 23 12"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></path>{" "}
+                                <circle
+                                  cx="12"
+                                  cy="12"
+                                  r="3"
+                                  stroke="#000000"
+                                  stroke-width="2"
+                                  stroke-linecap="round"
+                                  stroke-linejoin="round"
+                                ></circle>{" "}
+                              </g>
+                            </svg>
+                          )}
+                        </div>
                       </label>
                       <input
-                        type="password"
+                        type={`${showP1 ? "text" : "password"}`}
                         name="confirmpassword"
                         id="confirmpassword"
                         placeholder="••••••••"
@@ -244,7 +434,7 @@ export default function Register() {
                         Your Mobile
                       </label>
                       <input
-                        type="text"
+                        type="number"
                         name="phone"
                         id="phone"
                         class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
